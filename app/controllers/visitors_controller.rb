9020a61cd12dfeda9eda91e0a653c666e41cae80
Laetitia Massa <laetitiamassa@gmail.com>
2015-05-30 21:37:13 +0200

    added pages and navigation

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,23 @@
 class VisitorsController < ApplicationController
+	def home
+	end
+
+	def workshops
+	end
+
+	def planning
+	end
+
+	def pass
+	end
+
+	def teachers
+	end
+
+	def hotel
+	end
+
+	def edition_2015
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'visitors#home'
   devise_for :users
   resources :users
+
+  get 'workshops' => 'visitors#workshops'
+  get 'planning' => 'visitors#planning'
+  get 'pass' => 'visitors#pass'
+  get 'teachers' => 'visitors#teachers'
+  get 'edition_2015' => 'visitors#edition_2015'
+  get 'hotel' => 'visitors#hotel'
 end
